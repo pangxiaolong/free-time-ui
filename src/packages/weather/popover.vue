bce7a9408f9fedf0192904e0d8e910af82c30d7d
pangxiaolong <xlongway@126.com>
2019-07-11 17:59:58 +0800

    :art: avatar art

--- src/packages/weather/popover.vue
+++ src/packages/weather/popover.vue
@@ -1,22 +1,52 @@
 <template>
-  <div class="popover-container" style="width:100px">
-    <div v-for="(item,index) in dayList" :key="index">
-      <div>{{item.date}}</div>123
+    <div class="popover-container" style="width:100%">
+        <div class="popover-list">
+            <div class="popover-item" v-for="(item,index) in dayList" :key="index">
+                {{item.date}}
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      dayList: [{ date: 11111 }, { date: 22222 }, { date: 3333333 }]
+    export default {
+        data() {
+            return {
+                dayList: [{
+                    date: '空气质量PM2.5'
+                }, {
+                    date: '湿度30%'
+                }, {
+                    date: '能见度200'
+                }, {
+                    date: '明天：晴，25C'
+                }]
+            };
+        },
+        methods: {}
     };
-  },
-  methods: {}
-};
 </script>
 <style lang="scss">
-.popover-container {
-    transition:height 2s, transform 2s;
-}
+    .popover-container {
+        transition: height 2s, transform 2s;
+
+        .popover-list {
+            position: absolute;
+            border-radius: 4px;
+            z-index: 10024;
+            background: #fff;
+            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+
+            .popover-item {
+                padding: 0 16px;
+                font-size: 14px;
+                line-height: 40px;
+                height: 40px;
+                margin-top: 4px;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                cursor: pointer;
+                transition: color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), border-color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), background 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), padding 0.15s cubic-bezier(0.645, 0.045, 0.355, 1);
+            }
+        }
+    }
 </style>
